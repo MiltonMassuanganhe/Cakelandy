b5000c5e3c41008eca5a0a69c0f71fd9e2dd75ac
mimito <milton.massuanganhe@gmail.com>
2022-08-10 16:30:53 +0200

    fixing backgound images

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,10 @@ module.exports = {
     icones: '#924363'
     },
     backgroundImage: {
-
       'bg_1': "url('/images/bg_1.jpg')",
       'bg_2': "url('/images/bg_2.jpg')",
       'bg_3': "url('/images/bg_3.jpg')",
-      'bg_4': "url('/images/bg_4.jpg')",
-
-      'bg_1': "url('images/bg_1.jpg')",
-      'bg_2': "url('images/bg_2.jpg')",
-      'bg_3': "url('images/bg_3.jpg')",
-      'bg_4': "url('images/bg_4.jpg')"
-
+      'bg_4': "url('/images/bg_4.jpg')"
     },
     backgroundPosition: {
       bottom: 'bottom',
@@ -44,7 +37,7 @@ module.exports = {
     fontFamily:{
       body: 'Nunito',
       titles: 'Great Vibes',
-      subtitles: 'Poppins',
+      subtitles: 'Poppins'
     }
   },
   plugins: [],
